be030694b8f715d5699e9a7bb53e824e224ff52e
Hugo de Paula <hugo@pucminas.br>
2024-02-28 11:14:54 -0300

    Added Hello World Example

---
+++ exemplos/Intro_00_AloMundo/doc/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"app","c":"AloMundo","l":"AloMundo()","u":"%3Cinit%3E()"},{"p":"app","c":"AloMundo","l":"main(String[])","u":"main(java.lang.String[])"}];updateSearchResults();
